4eb4ee1f552d9082cca39967d7fc1d832af0f8c1
“coadaro” <coadaro@gmail.com>
2022-04-28 18:10:05 -0400

    added links to state pages and quick escape key

--- assets/js/main.js
+++ assets/js/main.js
@@ -215,3 +215,36 @@
 		});
 
 })(jQuery);
+
+//added alert prompt for escape key
+//! currently commented out as it prompts on every newly opened page on the site
+
+// alert(`Please note, while using our website you may press the "Escape key" at any moment to quickly redirect from our website`);
+
+//added safety quick escape key
+
+function getAway() {
+	// Get away right now
+	window.open("http://weather.com", "_newtab");
+	// Replace current site with another benign site
+	window.location.replace('http://google.com');
+  }
+  
+  $(function() {
+  
+	$("#get-away").on("click", function(e) {
+	  getAway();
+	});
+  
+	$("#get-away a").on("click", function(e) {
+	  // allow the (?) link to work
+	  e.stopPropagation();
+	});
+  
+	$(document).keyup(function(e) {
+	  if (e.keyCode == 27) { // escape key
+		getAway();
+	  }
+	});
+  
+  });
